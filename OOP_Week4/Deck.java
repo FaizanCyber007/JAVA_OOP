a6840c45d64a41b7bea3b67aa9f5f3d9d677645f
Cyber-Terrorist-007 <faizanjaved246@gmail.com>
2023-09-25 15:42:10 +0500

    Created Cards and displayed complete Deck

---
+++ OOP_Week4/Deck.java
@@ -0,0 +1,24 @@
+public class Deck {
+    Card cards[] = new Card[52];
+    
+    String suits[] = {"Clubs", "Hearts", "Spades", "Diamond"};
+    String ranks[] = {"Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "Jack", "Queen", "King"};
+    
+    Deck() {
+        int counter=0;
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<13; j++) {
+                cards[counter++] = new Card(ranks[j], suits[i]);
+            }
+        }
+    }
+
+    public String toString() {
+        StringBuilder str = new StringBuilder();
+        
+        for(int i=0; i<52; i++) {
+            str.append(cards[i] + "\n");
+        }
+        return str.toString();
+    }
+}
